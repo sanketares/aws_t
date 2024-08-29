b6084c6b5e951750aa298b9f03666fa631a804f9
sanket406 <sanketares@gmail.com>
2024-08-29 02:23:34 -0700

    add

--- main.tf
+++ main.tf
@@ -26,3 +26,13 @@ resource "aws_instance" "santest" {
   instance_type = "t2.micro"
   ami           = "ami-053b0d53c279acc90"
 }
+
+
+resource "aws_instance" "sanket_ins" {
+  instance_type = "t2.micro"
+  ami           = "ami-053b0d53c279acc90"
+  tags = {
+    Name = "sanket"
+  }
+
+}
